e94b893630219eda1de98998356dfc2ff42e1a0d
Christian <111076892+i-christian@users.noreply.github.com>
2024-06-19 17:28:07 +0200

    feat: Pipes - shell redirections added - example usage: Cargo.lock Cargo.toml

--- simple_shell/src/main.rs
+++ simple_shell/src/main.rs
@@ -2,7 +2,7 @@ use std::{
     env,
     io::{stdin, stdout, Write},
     path::Path,
-    process::Command,
+    process::{Child, Command, Stdio},
 };
 
 fn main() {
@@ -28,27 +28,58 @@ fn main() {
         //running program
         let mut input = String::new();
         stdin().read_line(&mut input).expect("failed to read input");
-        let mut parts = input.trim().split_whitespace();
-        let command = parts.next().expect("Failed to split command");
-        let args = parts;
 
-        match command {
-            "cd" => {
-                let new_dir = args.peekable().peek().map_or("/", |x| *x);
-                let root = Path::new(new_dir);
-                if let Err(e) = env::set_current_dir(&root) {
-                    eprintln!("{}", e);
+        let mut commands = input.trim().split(" | ").peekable();
+        let mut previous_command = None;
+
+        while let Some(command) = commands.next() {
+            let mut parts = command.trim().split_whitespace();
+            let command = parts.next().expect("Failed to split command");
+            let args = parts;
+
+            match command {
+                "cd" => {
+                    let new_dir = args.peekable().peek().map_or("/", |x| *x);
+                    let root = Path::new(new_dir);
+                    if let Err(e) = env::set_current_dir(&root) {
+                        eprintln!("{}", e);
+                    }
+
+                    previous_command = None;
+                }
+                "exit" => return,
+                command => {
+                    let stdin = previous_command.map_or(Stdio::inherit(), |output: Child| {
+                        Stdio::from(output.stdout.expect("failed to read command"))
+                    });
+
+                    let stdout = if commands.peek().is_some() {
+                        Stdio::piped()
+                    } else {
+                        Stdio::inherit()
+                    };
+
+                    let output = Command::new(command)
+                        .args(args)
+                        .stdin(stdin)
+                        .stdout(stdout)
+                        .spawn();
+
+                    match output {
+                        Ok(output) => previous_command = Some(output),
+                        Err(e) => {
+                            previous_command = None;
+                            eprintln!("{}", e);
+                        }
+                    }
                 }
             }
-            "exit" => return,
-            command => {
-                if let Ok(mut child) = Command::new(command).args(args).spawn() {
-                    child.wait().expect("failed to await child");
-                    child.kill().expect("command couldnt be killed");
-                } else {
-                    println!("{}: command not found", command);
-                }
-            }
+        }
+
+        if let Some(final_command) = previous_command {
+            final_command
+                .wait_with_output()
+                .expect("failed to wait on child");
         }
     }
 }

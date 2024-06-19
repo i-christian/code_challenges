71c641bb9f2e4e49353a145ddaa3fca6505e80ee
Christian <111076892+i-christian@users.noreply.github.com>
2024-06-19 15:59:35 +0200

    Feat: Implement basic shell functionality - simple commands like Cargo.lock Cargo.toml README.md src target, total 20 -rw-r--r-- 1 christian christian  156 Jun 18 15:00 Cargo.lock -rw-r--r-- 1 christian christian   83 Jun 18 15:00 Cargo.toml -rw-r--r-- 1 christian christian   21 Jun 18 15:02 README.md drwxr-xr-x 2 christian christian 4096 Jun 18 15:00 src drwxr-xr-x 3 christian christian 4096 Jun 18 15:00 target, , /home/christian/rust_series/simple_shell are working

--- simple_shell/src/main.rs
+++ simple_shell/src/main.rs
@@ -1,3 +1,40 @@
+use std::{
+    io::{stdin, stdout, Write},
+    process::Command,
+};
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        let output = if cfg!(target_os = "windows") {
+            Command::new("cmd")
+                .args(["/C", "echo hello"])
+                .output()
+                .expect("failed to execute process")
+        } else {
+            Command::new("sh")
+                .arg("-c")
+                .arg("echo hello")
+                .output()
+                .expect("failed to execute process")
+        };
+
+        let _hello = output.stdout;
+
+        print!("$ ");
+        stdout().flush().expect("print error");
+
+        //running program
+        let mut input = String::new();
+        stdin().read_line(&mut input).expect("failed to read input");
+        let mut parts = input.trim().split_whitespace();
+        let command = parts.next().expect("Failed to split command");
+        let args = parts;
+
+        let mut child = Command::new(command)
+            .args(args)
+            .spawn()
+            .expect("failed to parse command");
+
+        child.wait().expect("failed to initialise child process");
+    }
 }

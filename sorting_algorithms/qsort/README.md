73819b55ec4e8f1169fef43a464153a70464da11
Christian <111076892+i-christian@users.noreply.github.com>
2024-07-08 18:59:35 +0200

    Implementation of quicksort - The algorithm - Test cases

---
+++ sorting_algorithms/qsort/README.md
@@ -0,0 +1,5 @@
+# Quick sort
+O(n log n) for average case
+O(n) for worse case
+
+Note: Quicksort has a smaller constant than merge sort. So if they’re both O(n log n) time, quicksort is faster. And quicksort is faster in practice because it hits the average case way more often than the worst case
---
+++ sorting_algorithms/qsort/src/lib.rs
@@ -0,0 +1,45 @@
+pub fn quicksort(mut array: Vec<i32>) -> Vec<i32> {
+    // Base case covers [] and [1]
+    if array.len() < 2 {
+        return array;
+    } else {
+        let pivot = array.remove(0);
+        let mut less = Vec::new();
+        let mut greater = Vec::new();
+
+        // populate arrays with values on the left and right side of the pivot
+        for i in array {
+            if i <= pivot {
+                less.push(i);
+            } else {
+                greater.push(i);
+            }
+        }
+
+        // reassemble the array
+        let mut sorted = quicksort(less);
+        sorted.push(pivot);
+        sorted.extend(quicksort(greater));
+
+        sorted
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_list() {
+        let list = vec![5, -100, 6, 2, 100, 0];
+        let result = quicksort(list);
+        assert_eq!(result, vec![-100, 0, 2, 5, 6, 100]);
+    }
+
+    #[test]
+    fn test_empty() {
+        let list = Vec::new();
+        let result = quicksort(list);
+        assert!(result.is_empty());
+    }
+}

a2bd3c70b9b4f81fb22836280c0c109ad94b094c
christian <111076892+i-christian@users.noreply.github.com>
2025-03-07 21:42:37 +0200

    feat: implement default option (no options provided)

--- ccwc/internal/count.go
+++ ccwc/internal/count.go
@@ -54,39 +54,6 @@ func Words(f *os.File) (int, error) {
 	return wordCount, nil
 }
 
-// CountBytes function handles the command for byte count
-func CountBytes(f *os.File) (int, error) {
-	scanner := bufio.NewScanner(f)
-	scanner.Split(bufio.ScanBytes)
-	byteCounts := 0
-
-	for scanner.Scan() {
-		byteCounts++
-	}
-
-	if err := scanner.Err(); err != nil {
-		return 0, err
-	}
-
-	return byteCounts, nil
-}
-
-// CountLines function handles the command for line count
-func CountLines(f *os.File) (int, error) {
-	scanner := bufio.NewScanner(f)
-	scanner.Split(bufio.ScanLines)
-	lineCount := 0
-	for scanner.Scan() {
-		lineCount++
-	}
-
-	if err := scanner.Err(); err != nil {
-		return 0, err
-	}
-
-	return lineCount, nil
-}
-
 // Characters function returns the total word count in a file
 func Characters(f *os.File) (int, error) {
 	charCount := 0
--- ccwc/main.go
+++ ccwc/main.go
@@ -2,27 +2,17 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"log/slog"
 	"os"
+	"strings"
 
 	count "ccwc/internal"
 )
 
-// define main for the application to recieve command line args ()
-func main() {
-	args := os.Args
-	if len(args) < 3 {
-		slog.Error("Not enough arguments")
-		os.Exit(1)
-	}
-
-	mappedCommands := make(map[string]func(*os.File) (int, error))
-	mappedCommands["-c"] = count.Bytes
-	mappedCommands["-l"] = count.Lines
-	mappedCommands["-w"] = count.Words
-	mappedCommands["-m"] = count.Characters
-
-	f, err := os.Open(args[2])
+// processFlags function accepts a file and a flag and calls the function that matches the flag to handle the file
+func processFlags(flag, fileName string) (int, error) {
+	f, err := os.Open(fileName)
 	if err != nil {
 		slog.Error("failed to open file", "error", err.Error())
 		os.Exit(1)
@@ -30,11 +20,54 @@ func main() {
 
 	defer f.Close()
 
-	result, err := mappedCommands[args[1]](f)
-	if err != nil {
-		slog.Error("An error occured", "error", err.Error())
-		os.Exit(1)
+	mappedCommands := make(map[string]func(*os.File) (int, error))
+	mappedCommands["-c"] = count.Bytes
+	mappedCommands["-l"] = count.Lines
+	mappedCommands["-w"] = count.Words
+	mappedCommands["-m"] = count.Characters
+
+	result, err := mappedCommands[flag](f)
+	return result, err
+}
+
+// define main for the application to recieve command line args ()
+func main() {
+	args := os.Args
+	if len(args) <= 1 {
+		log.Fatal("Not enough arguments")
 	}
 
-	fmt.Printf("%d %s\n", result, args[2])
+	countSlice := make([]int, 0)
+	defaultFlags := []string{"-l", "-w", "-c"}
+	var fileName string
+
+	if len(args) > 1 {
+		if len(args) == 2 && !strings.Contains(args[1], "-") {
+			fileName = args[1]
+			for _, flag := range defaultFlags {
+				result, err := processFlags(flag, fileName)
+				if err != nil {
+					slog.Error("An error occured", "error", err.Error())
+					break
+				}
+				countSlice = append(countSlice, result)
+			}
+		} else if len(args) == 3 {
+			fileName = args[2]
+			result, err := processFlags(args[1], fileName)
+			if err != nil {
+				slog.Error("An error occured", "error", err.Error())
+			}
+
+			countSlice = append(countSlice, result)
+		} else {
+			fmt.Println("Too many command line arguments")
+			return
+		}
+	}
+
+	for i := range countSlice {
+		fmt.Print("   ", countSlice[i])
+	}
+	fmt.Println(" ", fileName)
 }
